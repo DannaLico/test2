a5c3228d3122b1383b3ede92087145169e041b24
Administrator <Administrator@WUYTG43RVAYT2GZ>
2020-01-03 12:01:13 +0800

    123

---
+++ Test/src/testone/Test2.java
@@ -0,0 +1,12 @@
+package testone;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("abc");
+		String name="danna";
+
+		System.out.println(name);
+		String name2="lico";
+		System.out.println("lico");
+	}
+}
