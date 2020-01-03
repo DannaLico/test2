0078a4243a5707bf6048a72f993f8f554374179e
Administrator <Administrator@WUYTG43RVAYT2GZ>
2020-01-03 11:25:00 +0800

    123

--- Test/src/testone/Test1.java
+++ Test/src/testone/Test1.java
@@ -3,7 +3,8 @@ package testone;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("abc");
+		String name0="danna";
+		System.out.println(name0);
 		String name="danna";
-		System.out.println(name);
 	}
 }
